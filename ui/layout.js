0ed9ee601f68149722e6138316832b1e6e94df83
Adam Terlson <adam.terlson@gmail.com>
2017-12-20 01:55:48 +0100

    Layout example

--- ui/layout.js
+++ ui/layout.js
@@ -1,33 +1,30 @@
 /* @flow */
 
-import * as React from 'react'
-import FlexView from 'app/ui/presentational/FlexView'
-import { flowRight as compose } from 'lodash'
-import { connect, type Connector } from 'react-redux'
-import * as storeActions from 'app/store/actions'
-import * as routeKeys from 'app/ui/routes/keys'
-import { mapAllConversations } from 'app/ui/mappers'
-import data from 'app/ui/enhancers/data'
-import pagination, { type PaginationProps } from 'app/ui/enhancers/pagination'
-import * as apiActions from 'app/saga/api/actions'
-import ConversationTeaser from 'app/ui/connected/ConversationTeaser'
-import Container from 'app/ui/presentational/Container'
-import SwipeableList from 'app/ui/presentational/SwipeableList'
-import FloatingButton from 'app/ui/presentational/FloatingButton'
+import ...
 
-type ConnectedPropTypes = {
-    conversations: TConversation[],
-    onSwipe: (item: any) => mixed,
-    onCreateMessagePress: () => mixed,
-}
+type OwnProps = { ... }
+type ConnectProps = { ... }
+
+const connector: Connector<OwnProps, ConnectProps> = connect(mapConversations, { ... })
+
+const hoc = compose(
+    connector,
+    data({
+        onMount: apiActions.reqConversations,
+    }),
+    pagination('conversations', {
+        limit: 10,
+        onLoadNextPage: apiActions.reqConversations,
+    }),
+)
 
 const renderItem = (item: { id: string }) => (
     <Container padding>
-        <ConversationTeaser id={item.id} />
+        <ConversationItem id={item.id} />
     </Container>
 )
 
-const AllConversations = (props: ConnectedPropTypes & PaginationProps) => (
+const ConversationList = (props: ConnectProps & PaginationProps) => (
     <FlexView>
         <SwipeableList
             data={props.conversations}
@@ -40,21 +37,4 @@ const AllConversations = (props: ConnectedPropTypes & PaginationProps) => (
     </FlexView>
 )
 
-const conversationsConnector: Connector<{}, ConnectedPropTypes> = connect(mapAllConversations, {
-    onSwipe: item => apiActions.reqConversationDelete(item.serverId),
-    onCreateMessagePress: () =>
-        storeActions.navigate({
-            routeName: routeKeys.CreatePrivateConversation,
-        }),
-})
-
-export default compose(
-    conversationsConnector,
-    data({
-        onMount: apiActions.reqConversations,
-    }),
-    pagination('conversations', {
-        limit: 10,
-        onLoadNextPage: apiActions.reqConversations,
-    }),
-)(AllConversations)
+export default hoc(AllConversations)
